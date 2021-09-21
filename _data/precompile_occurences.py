a31f9b9d6f394344c51d1e33dc272d3b7c98b475
- <>
2021-09-21 22:56:49 +0200

    edit

--- _data/precompile_occurences.py
+++ _data/precompile_occurences.py
@@ -1,11 +1,12 @@
 import pandas as pd
+import yaml
+
 lexicon_filename="comma_separated_TBESH_formatted.csv"
 index_filename="temp.csv"
 biblebooks_filename="biblebooks.csv"
 
 def force_to_int(value):
-    """Given a value, returns the value as an int if possible.
-    Otherwise returns None.
+    """Converts a value to int or returns None.
     """
     try:
         return int(value)
@@ -23,12 +24,15 @@ with open("../_includes/template_link.html","r") as fin:
     template_link=fin.read().rstrip()
 with open("../_includes/template_book.html","r") as fin:
     template_book=fin.read().rstrip()
+with open("../_config.yml", "r") as fin:
+    dic=yaml.safe_load(fin)
 
 lexicon["occurences"]=lexicon["occurences"].apply(lambda x: x.split(","))
 one_occurence_by_line=lexicon[["strongNumber","occurences"]].explode("occurences")
 one_occurence_by_line["occurences"]=one_occurence_by_line["occurences"].apply(force_to_int)
 one_occurence_by_line=one_occurence_by_line.sort_values(by=["strongNumber","occurences"])
 
+template_link=template_link.replace("{{{{site.readersite}}}}", dic.readersite).replace("{{{{site.readersiteprefix}}}}", dic.readersiteprefix)
 links_fetched = one_occurence_by_line.merge(index,left_on="occurences",right_on="number").dropna(axis=0)
 links_fetched["link"]=links_fetched[["number","chapter","verse"]].apply(lambda x: template_link.format(x["number"],x["chapter"],x["verse"]).format(x["chapter"],x["verse"]), axis=1)
 links_fetched=links_fetched.drop(["occurences","number","chapter","verse"], axis=1)
